78460147ac89112515ca33f6f7796c027636ce73
Anthony <Anthonyrichn@gmail.com>
2024-04-30 09:07:59 +0200

    Added Files for documentation

---
+++ src/main/java/com/example/asielzoekersopt/IData.java
@@ -0,0 +1,6 @@
+package com.example.asielzoekersopt;
+
+public interface IData {
+    void requestData();
+    void modifyData();
+}
---
+++ src/main/java/com/example/asielzoekersopt/IManage.java
@@ -0,0 +1,7 @@
+package com.example.asielzoekersopt;
+
+public interface IManage {
+    public void add();
+    public void delete();
+    public void modify();
+}
